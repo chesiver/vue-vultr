c2c01e5e23267c373b2409a91dae4cc956c5f08c
Adam Liu <yidong.liu@oracle.com>
2021-01-29 11:03:26 -0800

    change config name

--- vultr-show/vue.config.js
+++ vultr-show/vue.config.js
@@ -3,10 +3,4 @@ module.exports = {
         // other webpack options to merge in ...
     },
     // devServer Options don't belong into `configureWebpack`
-    devServer: {
-        host: '0.0.0.0',
-        hot: true,
-        disableHostCheck: true,
-        public: 'www.chesiver.top:8080',
-    },
 }
