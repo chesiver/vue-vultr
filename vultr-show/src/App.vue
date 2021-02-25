e7a567a0bac7ed9960be5cdd6d59682da9e6718f
Adam Liu <yidong.liu@oracle.com>
2021-02-24 20:16:38 -0800

    add router

--- vultr-show/src/App.vue
+++ vultr-show/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <h1>Adam Liu's Space</h1>
+  <p>
+    <router-link to="/home">Go to Home</router-link>
+    <br />
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <transition>
+    <router-view></router-view>
+  </transition>
   <kinesis-container event="move">
     <kinesis-element strength="10" type="rotate">
-      <img 
-      width="600" height="400"
-      src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/120194685_3145559558885908_1170017041477723343_o.png?_nc_cat=106&ccb=3&_nc_sid=8024bb&_nc_ohc=kuykmYcf6RAAX99rhu6&_nc_ht=scontent-nrt1-1.xx&oh=6bd14cc63b1373247ae2a55779e44910&oe=605BF7E8" alt="logrocket logo" />
+      <img
+        width="300"
+        height="200"
+        src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/120194685_3145559558885908_1170017041477723343_o.png?_nc_cat=106&ccb=3&_nc_sid=8024bb&_nc_ohc=kuykmYcf6RAAX99rhu6&_nc_ht=scontent-nrt1-1.xx&oh=6bd14cc63b1373247ae2a55779e44910&oe=605BF7E8"
+        alt="logrocket logo"
+      />
     </kinesis-element>
   </kinesis-container>
 </template>
--- vultr-show/src/main.js
+++ vultr-show/src/main.js
@@ -1,7 +1,21 @@
-import { createApp} from 'vue'
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import VueKinesis from 'vue-kinesis'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
+const routes = [
+    { path: '/home', component: Home },
+    { path: '/about', component: About },
+]
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
 
 const app = createApp(App);
+app.use(router);
 app.use(VueKinesis);
 app.mount('#app');
